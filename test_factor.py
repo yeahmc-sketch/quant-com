6c834697e52fdf650ee11b506f34e604404ac71f
yeahmc-sketch <yeahmc@qq.com>
2026-05-09 11:23:37 +0800

    Add test factor file

---
+++ test_factor.py
@@ -0,0 +1,31 @@
+process AND CART MAVG CONTIMER8
+from quentation views
+from authentication import
+from authentication.uri
+from authentication.code
+
+## Factory define Factory
+
+def factory(results):
+    return {
+        "factory_id": "https://github.com/yeahmc-sketch/quant-com",
+        "faths": [
+            {
+                "count": 100,
+                "data": [
+                    {
+                        "status": "12030",
+                        "enables": [
+                             {
+                                 "type": "format",
+                                 "id": "1",
+                                  "title": "Main",
+                                  "data": "http://api.example.com/v1/12030"
+
+                            }
+                        ]
+                }
+            ]
+          }
+      }
+}
